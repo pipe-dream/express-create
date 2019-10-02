e1a4049798357199838aa726da18b2569589f8dd
Anders Jurisoo <jurisoo@hotmail.com>
2019-10-02 16:06:17 +0200

    More view things

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+var mustacheExpress = require('mustache-express')
 const app = express()
 
 app.use(express.static('public'))
+app.engine('html', mustacheExpress())
+app.set('view engine', 'html')
+app.set('views', __dirname +  '/views')
 
 const port = 3000
 
