b133622f8d7a8e0058db25204f4baf6f2b998e4c
Anders Jurisoo <jurisoo@hotmail.com>
2019-10-05 09:27:16 +0200

    Basic sandbox writing

--- routes/builds.js
+++ routes/builds.js
@@ -1,8 +1,41 @@
 const express = require("express");
 const router = express.Router();
+var fs = require('fs-extra');
+var rimraf = require("rimraf");
+
+
+function cleanSandBox() {
+    rimraf("sandbox/project/", function () { console.log("done"); });
+}
+
+function writeFiles(files) {
+    files.forEach(file => writeFile(file))
+}
+
+function writeFile(file) {
+    fs.outputFile(`sandbox/project/${file.path}`, file.content, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    });
+}
+
+
+
+
+
+
+
+
+ 
+
+
 
 module.exports = router.post("/pipe-dream/api/build", async (req, res) => {
+    cleanSandBox();
+    const result = writeFiles(req.body.reviewFiles);
+    
     res.send({
-        message: "Cool, we built files with node..."
+
     })
 })
