a2619e42b97c175d4619a5a135f525c384097931
Anders Jurisoo <jurisoo@hotmail.com>
2019-10-05 09:33:29 +0200

    Added save message

--- routes/saves.js
+++ routes/saves.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = router.patch("/pipe-dream/api/save", async (req, res) => {
-    res.send(["new save mr"])
+    res.send(["Save not implemented yet..."])
 })
